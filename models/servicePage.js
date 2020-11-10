a4556ea2c44daef25f9285a167a60957714ee529
abhinav sharma <abhiut37@gmail.com>
2020-11-10 16:13:45 +0530

    changed the  port number

---
+++ models/servicePage.js
@@ -0,0 +1,52 @@
+const mongoose = require("mongoose");
+
+const serviceSchema = new mongoose.Schema({
+    user: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User"
+    },
+    name: {
+        required: true,
+        type: String
+    },
+    slug: {
+        required: true,
+        type:String
+    },
+    hasCustomView: {
+        type: Boolean,
+        default: false 
+    },
+    pricingCards: {
+        type: Object,
+        required:true
+    },  //this will contain three objects which will contain price and details of the 3 pricing cards
+    aboutService: {
+        type: String,
+        default: null
+    },  //will get from the text editor
+    procedure: {type: Object,default:null}, //this will contain key value pairs of the Procedure section where key will be the name array and value will be the names and other key will be the description and the array will be the description array
+    documentsRequired: {
+        type: String,
+        default: null
+    } ,
+    eligibility: {
+        type: String,
+        default:null
+    } ,
+    advantages: {
+        type: String,
+        default:null
+    },
+    steps: {
+        type: Object,
+        default:null
+    },
+    faq: {
+        type: Object,
+        default:null
+    } //This will also contain key value pairs of data name and data itself
+
+});
+
+module.exports = ServicePage = mongoose.model("servicePage", serviceSchema);
--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const PORT = 4000;
+const PORT = 8000;
 const path = require("path");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
@@ -12,6 +12,7 @@ const passportLocalMongoose = require("passport-local-mongoose");
 //auth finished
 const Blog = require("./models/blog");
 const app = express();
+const servicePage = require("./models/servicePage");
 //middlewares
 app.use(express.static(path.join(__dirname, "public")));
 app.set("view engine", "ejs");
@@ -20,12 +21,50 @@ app.use(
         extended: true,
     })
 );
+//mongo db
+const mongoURI =
+    "mongodb+srv://admin-Abhinav:admin-Abhinav@cluster0-fz1t0.mongodb.net/rapport";
+mongoose
+    .connect(mongoURI, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    })
+    .then(() => {
+        console.log("database has been connected");
+    });
+mongoose.set("useCreateIndex", true);
+//========================== Authentication start ==========================//
+//user schema
+const userSchema = new mongoose.Schema({
+    first_name: String,
+    last_name: String,
+    access: {
+        type: Boolean,
+        default: true
+    },
+    username: {
+        type: String,
+        
+    },
+    contact_number: {
+        type: Number,
+    },
+    date: {
+        type: Date,
+        default: Date.now()
+    },
+    type:String
 
+});
+userSchema.plugin(passportLocalMongoose);
+const User =  new mongoose.model("User", userSchema);
+passport.use(User.createStrategy());
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+//============== end authentication ====================== //
 app.get("/", (req, res) => {
     res.render("index")
-});
-app.get("/services/:name", (req, res) => {
-    res.render("pfReturn");
 });
 
 app.get("/blog", (req, res) => {
@@ -34,6 +73,40 @@ app.get("/blog", (req, res) => {
 app.get("/blogSingle", (req, res) => {
     res.render("post")
 });
+app.get("/adminPanel", (req, res) => {
+    res.render("adminPanel",{data:null})
+});
+
+app.get("/services/:slug", async(req, res) => {
+    let pageData = await servicePage.findOne({ slug: req.params.slug });
+    if (pageData) {
+        
+        res.render("servicePage",{...pageData._doc});
+    } else {
+         res.render("pfReturn");
+    }
+   
+});
+//    Post request to get all the service page info from the admin //
+app.post("/websiteData", async (req, res) => {
+    const { nameOfService, serviceurl, rapportStart, rapportSelect, rapportSuper, about_service, procedureName, procedureDescription, documents_required, eligibility,advantages , faqQuestion, faqAnswer,stepsName, stepsDescription } = req.body;
+    const servicePageObject = {
+        name: nameOfService,
+        slug: serviceurl,
+        pricingCards: { rapportStart, rapportSelect, rapportSuper },
+        aboutService: about_service,
+        procedure: { procedureName, procedureDescription },
+        documentsRequired: documents_required,
+        eligibility,
+        advantages,
+        steps:{stepsName, stepsDescription},
+        faq:{ faqQuestion,faqAnswer }
+    }
+    const newServicePage = new servicePage(servicePageObject);
+    await newServicePage.save();
+    res.render("adminPanel")
+});
+
 
 app.listen(PORT, () => {
     console.log(`Server started on PORT ${PORT}`);
